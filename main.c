86c2b87844f0c14677c9c1f7b70f13889da319a5
Novpa <agungnovpa@gmail.com>
2024-07-23 20:55:50 +0800

    Learn File Processing

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 
 FILE* fp;
 char myChar1;
-
+int i = 0;
 fp = fopen("myFirstFile.txt", "r");
 
 if(fp != NULL) {
-    
-    printf("The file was successfully opened!\n\n");
-    myChar1 = fgetc(fp);
-    printf("the character that was red is: %c\n", myChar1);
-    // fgetc(stdin);
-    getchar();
+
+    while(!feof(fp)) {
+      myChar1 = fgetc(fp);
+      printf("%c", myChar1);
+      if(myChar1 == '\n')
+      i++;
+    }
+      printf("\nThere are %d rows in it", i+1);
+    fclose(fp);
   }
-
-
   return 0;
 }
